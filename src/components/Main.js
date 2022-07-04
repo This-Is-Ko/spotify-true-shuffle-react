5ce0d6bb64e9d3f7e72aaa0cf21a658b1c719453
Kohei Hashmi <khashmi@student.unimelb.edu.au>
2022-07-04 16:32:15 +1000

    Homepage updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -27,24 +27,25 @@ const Main = () => {
                 backgroundImage: "linear-gradient(-45deg, rgb(86, 11, 179) -5%, rgb(167, 13, 105)",
                 height: "500px"
               }}>
-                <h1>True Shuffle</h1>
+                <h1 className={"textOnBackground"}>True Shuffle</h1>
               </div>
+              <h1>Get started</h1>
               <div className={"homepageIconHolder"}>
                 <div>
                   <img className={"homepageIcon"}
-                       src={"https://4197r62cmrjs32n9dndpi2o1-wpengine.netdna-ssl.com/wp-content/uploads/2020/07/square-placeholder.jpg"}/>
+                       src={"https://4197r62cmrjs32n9dndpi2o1-wpengine.netdna-ssl.com/wp-content/uploads/2020/07/square-placeholder.jpg"} alt={"login image"}/>
                   <h2>Login to Spotify</h2>
                   <p></p>
                 </div>
                 <div>
                   <img className={"homepageIcon"}
-                       src={"https://4197r62cmrjs32n9dndpi2o1-wpengine.netdna-ssl.com/wp-content/uploads/2020/07/square-placeholder.jpg"}/>
+                       src={"https://4197r62cmrjs32n9dndpi2o1-wpengine.netdna-ssl.com/wp-content/uploads/2020/07/square-placeholder.jpg"} alt={"shuffle image"}/>
                   <h2>Select playlist to shuffle</h2>
                   <p></p>
                 </div>
                 <div>
                   <img className={"homepageIcon"}
-                       src={"https://4197r62cmrjs32n9dndpi2o1-wpengine.netdna-ssl.com/wp-content/uploads/2020/07/square-placeholder.jpg"}/>
+                       src={"https://4197r62cmrjs32n9dndpi2o1-wpengine.netdna-ssl.com/wp-content/uploads/2020/07/square-placeholder.jpg"} alt={"music image"}/>
                   <h2>Start listening</h2>
                   <p></p>
                 </div>
