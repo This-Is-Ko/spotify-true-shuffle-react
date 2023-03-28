99df0f7cf64b1ac235e5a5f8ca0b2e484cb8ae3c
Kohei Hashmi <khashmi@student.unimelb.edu.au>
2023-03-28 18:42:31 +1100

    Created generic modal for how to

---
+++ src/components/howToComponents/HowToEntries.js
@@ -0,0 +1,17 @@
+export const HowToShuffleEntry = [
+    "Select a playlist below. If the playlist you want isn't appearing, ensure it is either a playlist you created or are following",
+    "A new playlist will be created named \"[Shuffled]\" and your playlist name e.g. [Shuffled] My Playlist",
+    "If you have already shuffled the playlist before, the existing shuffled one will be replaced (Prevents build up of playlists in your account)",
+    "Enjoy!"
+]
+
+export const HowToDeletePlaylistsEntry = [
+    "Just click the below button to remove all shuffled playlists from your Spotify account. This will not affect any of your existing playlists and only delete playlists named \"[Shuffled] ...\"",
+    "Note: You don't need to remove an existing shuffled playlist to re-shuffle it"
+]
+
+export const HowToShareLikedTracksEntry = [
+    "Click \"Start\"",
+    "Enter a playlist name or leave as default",
+    "A new playlist will be created with all the songs currently in your Liked Songs"
+]
