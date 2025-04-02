fb1fb77f138cbeab8a985f3e56ed03d7937733e8
This-Is-Ko <52279273+This-Is-Ko@users.noreply.github.com>
2025-04-02 17:03:26 +1100

    Updated faq

--- src/pages/FAQPage.js
+++ src/pages/FAQPage.js
@@ -7,7 +7,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 const faqs = [
     {
         "question": "What is True Shuffle?",
-        "answer": "I created this website with the goal to have a way of properly shuffling Spotify playlists. There are also features to share your Liked Songs and Anaylse your library.",
+        "answer": "I created this website with the goal to have a way of properly shuffling Spotify playlists. There are also features to share your Liked Songs and Analyse your library.",
         "id": "q1"
     },
     {
@@ -16,36 +16,47 @@ const faqs = [
         "id": "q2"
     },
     {
-        "question": "Can I shuffle my Liked Songs?",
-        "answer": "Yes, you can shuffle your Liked Songs on the website. Simply select 'Liked Songs' on the shuffle page.",
+        "question": "Can I shuffle any Spotify playlist?",
+        "answer": "Yes, you can use True Shuffle with any Spotify playlist that you have access to. However, ensure you have Spotify Premium otherwise Spotify's shuffle cannot be disabled.",
         "id": "q3"
     },
     {
-        "question": "Can I shuffle any Spotify playlist?",
-        "answer": "Yes, you can use True Shuffle with any Spotify playlist that you have access to. However ensure you have Spotify Premium otherwise Spotify's shuffle cannot be disabled.",
+        "question": "Can I shuffle my Liked Songs?",
+        "answer": "Yes, you can shuffle your Liked Songs on the website. Simply select 'Liked Songs' on the shuffle page.",
         "id": "q4"
+    },
+    {
+        "question": "Can I shuffle local files?",
+        "answer": "No, Spotify does not allow local files to be accessed or added to playlists through the API. If your playlist contains local files, they will be skipped during the shuffle process.",
+        "id": "q5"
+    },
+    {
+        "question": "What is the maximum playlist length?",
+        "answer": "Spotify officially limits playlists to 10,000 tracks, but in some cases, playlists can hold up to 11,000 tracks. True Shuffle will fully shuffle your entire playlist, but only the first 10,000 tracks will be saved in the new shuffled playlist.",
+        "id": "q6"
     },
     {
         "question": "Why is my shuffle taking so long?",
         "answer": "As each call to Spotify is limited to 100 tracks, the larger your playlist is, the longer it takes to retrieve all tracks then add them to the shuffled playlist.",
-        "id": "q5"
+        "id": "q7"
     },
     {
         "question": "Is True Shuffle an official Spotify product?",
         "answer": "No, True Shuffle is an independent website and is not affiliated with Spotify.",
-        "id": "q6"
+        "id": "q8"
     },
     {
         "question": "What user data is stored?",
-        "answer": "The only user data that is stored is the Spotify user ID, access token to retrieved Liked Songs and counters for shuffles. Cookies are used to maintain logged in sessions. No passwords are stored and all authentication is handled by Spotify.",
-        "id": "q7"
+        "answer": "The only user data that is stored is the Spotify user ID, access token to retrieve Liked Songs, and counters for shuffles. Cookies are used to maintain logged-in sessions. No passwords are stored and all authentication is handled by Spotify.",
+        "id": "q9"
     },
     {
         "question": "How can I give feedback?",
-        "answer": "I'd love to hear from you if you have any feedback or suggestions. You contact me with the email listed on the About page or click on the Contact link in the footer.",
-        "id": "q8"
-    },
+        "answer": "I'd love to hear from you if you have any feedback or suggestions. You can contact me with the email listed on the About page or click on the Contact link in the footer.",
+        "id": "q10"
+    }
 ];
+
 
 const renderAnswer = (answer) => {
   return answer.split('\n').map((line, index) => <p key={index}>{line}</p>);
